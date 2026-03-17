a1ddbe0e4068ae9e47b0dd2cf6832eb04a7c43b9
yuhandbcontext <yuhandbcontext@noreply.gitcode.com>
2026-03-17 22:07:33 +0800

    弹跳系数 ，生成实例

---
+++ assets/Scripts/GameManager.ts
@@ -0,0 +1,41 @@
+import { _decorator, Component, find, Game, instantiate, Label, macro, Node, Prefab, random, randomRange, randomRangeInt } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('GameManager')
+export class GameManager extends Component {
+    private static _instance: GameManager;
+    @property([String])
+    workNameArray: string[] = [];
+
+    @property(Prefab)
+    workLabelPrefab: Prefab;
+    //使用过的索引
+    private usedIndex: Set<number> = new Set<number>();
+
+    public static get instance(): GameManager {
+        return GameManager._instance;
+    }
+    protected onLoad(): void {
+        if (!GameManager._instance) {
+            GameManager._instance = this;//这里的this是 加载场景的时候 挂载到节点的时候实例化的对象
+        }
+    }
+    protected start(): void {
+        // this.schedule(this.instantiateLabel,0.5, 1, macro.REPEAT_FOREVER);
+        this.schedule(this.instantiateLabel.bind(this), 1, macro.REPEAT_FOREVER);
+    }
+    private instantiateLabel() {
+        if (this.usedIndex.size == this.workNameArray.length) return;
+
+        let rd: number = randomRangeInt(0, this.workNameArray.length);
+        if (this.usedIndex.has(rd)) return;
+        this.usedIndex.add(rd);
+        let worklabelNode = instantiate(this.workLabelPrefab);
+        worklabelNode.getComponent(Label).string = this.workNameArray[rd];
+        worklabelNode.parent = find("Canvas/WorkLabelList");//设置父节点
+        worklabelNode.setPosition(randomRange(-320, 320), 670);
+    }
+
+}
+
+
